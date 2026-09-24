85ef50f38dee09caf944f484118d8d1a12db35d3
Satveer-joeeya <Satveerjoeeya8824@gmail.com>
2026-09-24 16:18:16 +0530

    complete program

--- student_grade.py
+++ student_grade.py
@@ -1,7 +1,18 @@
 import pandas as pd
-data={"name":[],"marks":[]}
+data={"name":[],"marks":[],"grade":[]}
 num=int(input("Enter no. of student : "))
 for i in range(1,num+1):
     print("\n Enter detail of student no.",i)
     data["name"].append(input("Enter name of student : "))
     data["marks"].append(int(input("Enter marks of student : ")))
+for i in data["marks"]:
+    if i>=60 and i<=100:
+        data["grade"].append("First")
+    elif i>=45:
+        data["grade"].append("Second")
+    elif i>=33:
+        data["grade"].append("Third")
+    else:
+        data["grade"].append("Fail")
+df=pd.DataFrame(data)
+print(df.sort_values(by="name" ,ascending=True))
